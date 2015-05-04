4315e9f97ed0e0dc40bc2a6c5f6439cbd6835230
David Rios <davidrivers93@gmail.com>
2015-05-04 18:18:47 +0200

    commit win

---
+++ build_win64 - copia/trabajo/CMakeFiles/dorsales.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dorsales.dir/dorsales.cpp.obj"
+  "dorsales.pdb"
+  "dorsales.exe"
+  "dorsales.exe.manifest"
+  "libdorsales.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dorsales.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
