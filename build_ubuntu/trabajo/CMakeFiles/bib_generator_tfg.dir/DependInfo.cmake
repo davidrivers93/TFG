5058877e65d33428d8ff4120ec62dd87dac9413a
dariobe <davidrivers93@gmail.com>
2015-06-14 08:29:37 -0700

    Sync

---
+++ build_ubuntu/trabajo/CMakeFiles/bib_generator_tfg.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/david/Desktop/tfg/tfg/fuentes/trabajo/bib_generator_tfg.cpp" "/home/david/Desktop/tfg/tfg/build_ubuntu/trabajo/CMakeFiles/bib_generator_tfg.dir/bib_generator_tfg.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/david/Desktop/tfg/tfg/build_ubuntu/trabajo/CMakeFiles/utilities.dir/DependInfo.cmake"
+  "/home/david/Desktop/tfg/tfg/build_ubuntu/trabajo/CMakeFiles/functions.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/usr/local/include"
+  "/opt/local/include/mysql55-connector-cpp/cppconn"
+  "/opt/local/include"
+  "/opt/local/include/tesseract"
+  "/opt/local/include/leptonica"
+  "/opt/local/include/freetype2"
+  "/opt/local/include/libpng16"
+  "/opt/local/include/pixman-1"
+  "/opt/X11/include/cairo"
+  "/home/david/Desktop/tfg/tfg/fuentes"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build_ubuntu/trabajo/CMakeFiles/bib_generator_tfg.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bib_generator_tfg.dir/bib_generator_tfg.cpp.o"
+  "bib_generator_tfg.pdb"
+  "bib_generator_tfg"
+  "bib_generator_tfg.pdb"
+  "CMakeFiles/CMakeRelink.dir/bib_generator_tfg"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bib_generator_tfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
