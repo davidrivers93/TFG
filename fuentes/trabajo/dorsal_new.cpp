87149e1c1b01e259f3ca6e8f5c37a47b83489c76
David Rios <davidrivers93@gmail.com>
2015-06-14 12:53:16 +0200

    Add result to database

--- fuentes/trabajo/dorsal_new.cpp
+++ fuentes/trabajo/dorsal_new.cpp
@@ -40,12 +40,15 @@ using namespace std;
 using namespace cv;
 
 void computeContourDepth(const std::vector<Vec4i> & hierarchy, std::vector<int> & depth);
-void calculate(set<string> images, int contador);
+void calculate(set<string> images, int contador,database_mng & database );
 void create_txt_file();
 std::string getOsName();
 int showfiles(set<string> images);
 cv::Point2f blobCenter(const std::vector<cv::Point> & points);
 int numberOfSons(const std::vector<Vec4i> & hierarchy, int id);
+void list_races(std::vector<string> & list_races, database_mng & database);
+void add_result_db(std::string result, database_mng & database, std::string imagename);
+std::string search_number(std::string result);
 
 int main(int argc, char **argv) {
 
@@ -54,6 +57,9 @@ int main(int argc, char **argv) {
 	CImgDisplay disp_cimg;
 
 	const bool help = cimg_option("-h", false, 0);
+
+	database_mng database;
+
 
 	std::string input = "prueba.txt"; //argv[optind];
 
@@ -67,13 +73,19 @@ int main(int argc, char **argv) {
 			exit(0);
 	}
 
+	database.connect();
+
+
+
 	create_txt_file();
+
 	std::vector<string> vector_list_races;
+	list_races(vector_list_races, database);
 	set<string> images = isImages(input);
 
 	int contador = showfiles(images);
-
-	calculate(images, contador);
+	std::cerr << "LLEGO" << endl;
+	calculate(images, contador,database);
 
 	if (contador == 0) {
 		std::cout << "No hay ninguna imagen a procesar. \n";
@@ -82,6 +94,150 @@ int main(int argc, char **argv) {
 
 	return 0;
 
+}
+
+void list_races(std::vector<string> & list_races, database_mng & database) {
+
+	string db_races = "prueba";
+	database.switchDb(db_races);
+	database.database_name = db_races;
+	database.execute("SELECT * FROM races");
+	int index_bbdd_races = 0;
+	while (database.fetch()) {
+		std::cout << " Race " << database.print(1) << " -> " << database.print(2) << " " << database.print(3) << " " << database.print(4) << "\n";
+		index_bbdd_races++;
+	}
+	std::cerr << "En el caso de que la carrera no este metida en la base de datos escribe 0." << endl;
+	std::cerr << "Elige la carrera: \n";
+	std::cerr << "El indice es " << index_bbdd_races << endl;
+	string index_race;
+	std::cin >> index_race;
+
+	if(index_race == "0"){
+
+		std::cout << "Introduce el nombre de la carrera : ";
+		string new_race_name;
+		std::cin >> new_race_name;
+		database.race_data_query.race_data = new_race_name;
+		std::cerr << "Introduce la fecha de la carrera (formato dd-mm-aa 31-07-15 ) : ";
+		string new_race_date;
+		std::cin >> new_race_date;
+		database.race_data_query.race_data = new_race_date;
+		std::cerr << "Introduce la distancia de la carrera (en metros) : ";
+		string distance_string;
+		std::cin >> distance_string;
+		int distance;
+		istringstream ( distance_string ) >> distance;
+		database.race_data_query.distance = distance;
+		index_bbdd_races++;
+
+		stringstream new_race_table;
+		new_race_table << new_race_date.at(0) << new_race_date.at(1) << new_race_name.at(0) << new_race_name.at(1) << new_race_date.at(6) << new_race_date.at(7);
+		string new_race_tables = new_race_table.str();
+		string race_ins = "ins" + new_race_tables;
+		database.prepare("INSERT INTO prueba.races (idraces, races_name, races_date, race_dbname, race_distance, race_ins) VALUES (?, ?, ?, ?)");
+		database.setInt(1,index_bbdd_races);
+		database.setString(2,new_race_name);
+		database.setString(3,new_race_date);
+		database.setString(4,new_race_tables);
+		database.setInt(5,distance);
+		database.setString(6,race_ins);
+
+
+		//actualizo struct
+		database.race_data_query.race_data = new_race_name;
+		database.race_data_query.date_data =  new_race_date;
+		database.race_data_query.tablen_data = new_race_tables;
+		database.race_data_query.distance = distance;
+		database.race_data_query.ins_table = race_ins;
+
+		database.execute();
+
+
+	}
+	else{
+
+		string query_race = "Select * FROM races WHERE idraces= \'" + index_race + "\'";
+		database.execute(query_race);
+		database.fetch();
+		database.race_data_query.race_data = database.print(2);
+		database.race_data_query.date_data = database.print(3);
+		database.race_data_query.tablen_data = database.print(4);
+		string temp = database.print(5);
+		int distance;
+		istringstream ( temp ) >> distance;
+		database.race_data_query.distance = distance;
+		database.race_data_query.ins_table = database.print(6);
+
+
+	}
+
+	//Compruebo si existe la tabla de la carrera
+
+	database.prepare("SELECT * FROM ?");
+	database.setString(1, database.race_data_query.tablen_data);
+	database.execute();
+
+	string prueba_create = "CREATE TABLE " + database.race_data_query.tablen_data +  "(`dorsal` INT NOT NULL,`path_img` VARCHAR(45) NOT NULL)";
+	database.execute(prueba_create);
+	std::cout << "He creado la tabla. \n";
+
+
+	//Compruebo si existe tabla de inscritos
+	database.prepare("SELECT * FROM ?");
+	database.setString(1, database.race_data_query.tablen_data);
+	database.execute();
+
+
+	string prueba_create_ins = "CREATE TABLE " + database.race_data_query.ins_table +  "(`dorsal` INT NOT NULL,`Nombre` VARCHAR(45) NULL,`Apellidos` VARCHAR(45) NULL,`Marca` VARCHAR(45) NULL)";
+	database.execute(prueba_create_ins);
+	std::cout << "He creado la tabla. \n";
+
+
+}
+
+void add_result_db(std::string result, database_mng & database, std::string imagename){
+
+	std::cout << "Antes de formatear " << result << endl;
+
+	std::string resultado = search_number(result);
+
+	std::cout << "Resultado a enviar " << resultado << endl;
+
+	std::string query = "INSERT INTO " + database.race_data_query.tablen_data + "(dorsal, path_img) VALUES (" + resultado + ",/\'" + database.race_data_query.tablen_data + "/" + imagename + "\')";
+
+	std::cerr << "QUERY: " << query << endl;
+	database.prepare("INSERT INTO ? (dorsal,path_img) VALUES (?,?)");
+	database.setString(1,database.race_data_query.tablen_data);
+	int dorsal = std::stoi(resultado,0);
+	database.setInt(2,dorsal);
+	string path = "/" + database.race_data_query.tablen_data + "/" + imagename;
+	database.setString(1,path);
+
+	database.execute();
+
+
+}
+
+std::string search_number(std::string result){
+	std::string number;
+	std::cout << "Tess " << result <<endl;
+	std::vector <std::string> vector_posibles;
+	bool flag_estado = false;
+	int first_position = 0 ;
+
+	for(int i2=0 ; i2<result.length(); i2++){
+		if(!isspace(result[i2])) continue;
+
+		std::string temp = result.substr(first_position, i2);
+		vector_posibles.push_back(temp);
+
+	}
+
+	std::cout << "Tamaño posibles" << vector_posibles.size() << endl;
+	number = vector_posibles[0];
+
+	return number;
 }
 
 void create_txt_file() {
@@ -136,7 +292,7 @@ int showfiles(set<string> images) {
 	return contador;
 }
 
-void calculate(set<string> images, int contador) {
+void calculate(set<string> images, int contador,database_mng & database ) {
 
 	if (images.size()) {
 
@@ -152,23 +308,6 @@ void calculate(set<string> images, int contador) {
 			img_out_binarizacion.display("a", false);
 
 			//img.blur(0.8,0.8);
-
-			//Pasamos a grises
-
-			int EE = 20;
-
-			CImg<unsigned char> image_gray = img.get_RGBtoYCbCr().get_channel(0);
-			CImg<unsigned char> image_gray_cierre(image_gray);
-
-			image_gray_cierre.dilate(EE, EE).erode(EE, EE);
-
-			CImg<unsigned char> resta = image_gray - image_gray_cierre;
-
-			//resta.display("resta", false);
-
-			resta.threshold(200);
-
-			//resta.display("A", false);
 
 			Mat img_opencv = img_out_binarizacion.get_MAT();
 
@@ -528,6 +667,8 @@ void calculate(set<string> images, int contador) {
 
 				 api->End();
 				 pixDestroy(&image);
+
+				 add_result_db(outtext, database,imgname);
 			}
 
 		}
